52bb8d5a39336594b7f5a26cd53f57b8b91e8ecd
kr397 <kr397@cornell.edu>
2019-12-05 18:27:01 -0500

    Updated

--- MotionControl.py
+++ MotionControl.py
@@ -8,26 +8,46 @@ import RPi.GPIO as GPIO
 import time
 
 pir = 11    # PIR Motion sensor connected to pin 11 of RPi
-relay = 12  # Relay outputs connected to pin 12 of RPi
-relayTimer = 1000
+relay = 8  # Relay outputs connected to pin 12 of RPi
+relayTimer = 10
+relayOn = False
 
 GPIO.setmode( GPIO.BOARD )
 GPIO.setup( pir, GPIO.IN )
 GPIO.setup( relay, GPIO.OUT )
 
+print("Sensor Active, press Ctrl+C to exit")
+time.sleep(1)
+
 # Main loop
-while True:
-  if ( relayTimer > 0 ) :
-    GPIO.output( relay, 1 ) 
-    if ( GPIO.input( pir ) ) :
-      relayTimer = 1000
-    else :
-      relayTimer = relayTimer - 1
-  else :
-    if ( GPIO.input ( pir ) ) :
-      GPIO.output( relay, 1 )
-      relayTimer = 1000
-    else :
+try: 
+  while True:
+    if ( relayTimer > 0 ) :
       GPIO.output( relay, 0 )
+      relayOn = True 
+      if ( GPIO.input( pir ) ) :
+        relayTimer = 10
+        print("Motion")
+      else :
+        relayTimer = relayTimer - 1
+    else :
+      if ( GPIO.input ( pir ) ) :
+        GPIO.output( relay, 0 )
+        relayOn = True
+        relayTimer = 10
+        print("Motion")
+      else :
+        GPIO.output( relay, 1 )
+        relayOn = False
       
-  time.sleep(1)
+    time.sleep(1)
+    if ( relayOn ) :
+      print("LED on")
+    else:
+      print("LED off")
+    
+except: KeyboardInterrupt
+
+finally: 
+  GPIO.cleanup()
+  print("Program ended")
